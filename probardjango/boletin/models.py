0155e7b0f2d4e281f12ec40dd4d7a55be3e08d45
Roberto Morgado Luengo <RobertoMorgadoLuengo@MacBook-Pro-de-Roberto.local>
2016-08-02 23:32:17 +0200

    Added model Registrado and admin

--- probardjango/boletin/models.py
+++ probardjango/boletin/models.py
@@ -5,12 +5,16 @@ from django.db import models
 
 # Create your models here.
 
+
+# Creamos el modelo Registrado con los tipos de datos
 class Registrado(models.Model):
     nombre = models.CharField(max_length=120, blank=True, null=True)
     email = models.EmailField()
     codigo_postal = models.IntegerField()
-    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-    actualizado = models.DateTimeField(auto_now_add=False, auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False) # Solo se anade en el momento que se crea
+    actualizado = models.DateTimeField(auto_now_add=False, auto_now=True) # Solo se actualiza cuando se edita
+
+
 
     def __unicode__(self):  # Python 3 __str__
         return self.email
